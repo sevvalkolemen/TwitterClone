4aa6cb89dde7c1fa8588f04bd357d3e75f5786e8
Emincan Karasoy <emincannkarasoy@gmail.com>
2022-04-14 21:02:17 +0300

    Create splash screen template

---
+++ app/src/main/java/com/nesneyonelimli/twitterclone/ui/view/ApplicationActivity.java
@@ -0,0 +1,13 @@
+package com.nesneyonelimli.twitterclone.ui.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ApplicationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
